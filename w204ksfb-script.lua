01d47a4ed3e5a685237b0c6c352bf95b785e94d9
BlastingStone <60666096+BlastingStone@users.noreply.github.com>
2021-01-25 00:58:16 +0200

    Update w204ksfb-script.lua
    E

--- w204ksfb-script.lua
+++ w204ksfb-script.lua
@@ -1,2 +1,7 @@
+local function chat(msg)
+    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg,"All")
+end
+chat("im dumb and i cant read and BlastingStone just saved me from being banned")
+wait(0.4)
 game.Players.LocalPlayer:Kick("i just saved you from being fucking banned. do you know how to read? -BlastingStone")
 --game.Workspace.End.Badge.CFrame = game.Players.LocalPlayer.Character.Torso.CFrame --DONT USE THIS
