96821cea442ef8152494e2a47db109f96548736f
BlastingStone <60666096+BlastingStone@users.noreply.github.com>
2021-03-07 18:35:36 +0300

    Update animgrabberloader.lua

--- animgrabberloader.lua
+++ animgrabberloader.lua
@@ -3,5 +3,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaSt
 wait(0.2)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/animgrabberrevamp.lua"))()
 elseif game.PlaceId == 6283906180 then game.Players.LocalPlayer:Kick("An Anti-Cheat found an exception")
+else
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/animgrabberrevamp.lua"))()
 end
