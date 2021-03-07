65d1387746e320c6cd5c267fdf0aca5a03a8a82f
BlastingStone <60666096+BlastingStone@users.noreply.github.com>
2021-03-07 18:09:02 +0300

    Update animgrabberloader.lua

--- animgrabberloader.lua
+++ animgrabberloader.lua
@@ -7,9 +7,9 @@ local games = {
 
 local name = rawget(games, game.GameId)
 if name then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/antiantisteal.lua"))()
-  wait(1)
- --will insert another loadstring later
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/antiantisteal.lua"))()
+  wait(0.2)
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/animgrabberrevamp.lua"))()
 else
-  --will insert too
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/animgrabberrevamp.lua"))()
 end
