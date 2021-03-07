f2b5bcefa26a4fefff1bf0bad326ad63485f281b
BlastingStone <60666096+BlastingStone@users.noreply.github.com>
2021-03-07 18:13:36 +0300

    Update antiantisteal.lua

--- antiantisteal.lua
+++ antiantisteal.lua
@@ -1,12 +1,12 @@
 for i,v in pairs(getconnections(game:GetService("RunService").RenderStepped)) do
-    local f = v.Function
-    if f then
-        local c = getfenv(f)
-        if c then
-            local d = rawget(c, "script")
-            if d and d.Parent == nil and tostring(d) == "anti steal" then
-                v:Disable()
-            end
-        end
-    end
+local f = v.Function
+if f then
+local c = getfenv(f)
+if c then
+local d = rawget(c, "script")
+if d and d.Parent == nil then
+v:Disable()
+end
+end
+end
 end
