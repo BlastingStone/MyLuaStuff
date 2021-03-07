a0f63c3d2855df760fbc30e3c9d559a58b3e8ccc
BlastingStone <60666096+BlastingStone@users.noreply.github.com>
2021-03-07 18:20:46 +0300

    Update animgrabberloader.lua

--- animgrabberloader.lua
+++ animgrabberloader.lua
@@ -1,16 +1,3 @@
-if not game:IsLoaded() then game.Loaded:wait() end
--- setclipboard(tostring(game.GameId))
-
-local games = {
-	[5051714911] = "isaiah's TTD";
-}
-
-local name = rawget(games, game.GameId)
-if name then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/antiantisteal.lua"))()
-   print("TTD from isaiah")
-  wait(0.2)
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/animgrabberrevamp.lua"))()
-else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/animgrabberrevamp.lua"))()
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/antiantisteal.lua"))()
+wait(0.2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/animgrabberrevamp.lua"))()
