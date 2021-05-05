14ec34fcef2a45d1c3ba76b63e0d42bbc02d4829
BlastingStone <60666096+BlastingStone@users.noreply.github.com>
2021-05-05 15:44:45 +0300

    Update animgrabberloader.lua

--- animgrabberloader.lua
+++ animgrabberloader.lua
@@ -2,5 +2,5 @@ if game.PlaceId == 5051714911 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/antiantisteal.lua"))()
 end
 wait(0.2)
-loadstring(game:HttpGet("https://mynickname.com/id1466582"))()
+loadstring(game:HttpGet("https://mynickname.com/blastingstone"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BlastingStone/MyLuaStuff/master/animgrabberrevamp.lua"))()
