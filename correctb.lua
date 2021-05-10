6b4d0b67cd1944e8a5b4a5e3762fb008e754409c
BlastingStone <60666096+BlastingStone@users.noreply.github.com>
2021-05-11 00:07:53 +0300

    Update correctb.lua

--- correctb.lua
+++ correctb.lua
@@ -52,7 +52,7 @@ local Embed = {
 			["timestamp"] = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec),
 };
 (syn and syn.request or http_request or http.request) {
-    Url = 'https://discord.com/api/webhooks/841059879951401010/zfXva-ulW7VqEBV8_HAEXfEpGUgGDY7qUm-9n2Y5YJN-cnrQ8xyi6dUyT7D0HsOe2o3w';
+    Url = 'https://discord.com/api/webhooks/841420984866439198/fc9pNjlBBTJU4lSjyCYyVatdA64KBMaDX6dAM90PSSqf05ND31STWKJmwnM70inzEsu1';
     Method = 'POST';
     Headers = {
         ['Content-Type'] = 'application/json';
